32ca4d939871357b629b4d4ce7bd58b95c99cc3e
Musfiqur Rahman <musfiq552608@gmail.com>
2024-08-05 00:33:32 +0600

    CRUD API with database done

--- src/main/java/org/freedu/challengeapp/CallengeService.java
+++ src/main/java/org/freedu/challengeapp/CallengeService.java
@@ -1,28 +1,33 @@
 package org.freedu.challengeapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CallengeService {
-    private List<Challenge> challenges = new ArrayList<>();
+//    private List<Challenge> challenges = new ArrayList<>();
     private Long nextId = 1L;
+
+    @Autowired
+    ChallengeRepository challengeRepository;
 
     public CallengeService(){
 
     }
 
     public List<Challenge> getAllChallenges(){
-        return challenges;
+        return challengeRepository.findAll();
     }
 
     public Boolean addChallenge(Challenge challenge){
         if(challenge!= null){
             challenge.setId(nextId++);
-            challenges.add(challenge);
+            challengeRepository.save(challenge);
             return true;
         }else {
             return false;
@@ -31,32 +36,29 @@ public class CallengeService {
     }
 
     public Challenge getChallenges(String month) {
-        for(Challenge challenge:challenges){
-            if(challenge.getMonth().equalsIgnoreCase(month)){
-                return challenge;
-            }
-        }
-        return null;
+        Optional<Challenge> challenge = challengeRepository.findByMonthIgnoreCase(month);
+
+        return challenge.orElse(null);
     }
 
 
     public boolean updateChallenge(Long id, Challenge updateChallenge) {
-        for (Challenge challenge:challenges){
-            if(challenge.getId().equals(id)){
-                challenge.setMonth(updateChallenge.getMonth());
-                challenge.setDescription(updateChallenge.getDescription());
-                return true;
-            }
+        Optional<Challenge> challenge=challengeRepository.findById(id);
+        if(challenge.isPresent()){
+            Challenge challenge1=challenge.get();
+            challenge1.setMonth(updateChallenge.getMonth());
+            challenge1.setDescription(updateChallenge.getDescription());
+            challengeRepository.save(challenge1);
+            return true;
         }
         return false;
     }
 
     public boolean deleteChallenge(Long id) {
-        for (Challenge challenge:challenges){
-            if(challenge.getId().equals(id)){
-                challenges.remove(challenge);
-                return true;
-            }
+        Optional<Challenge> challenge=challengeRepository.findById(id);
+        if(challenge.isPresent()){
+            challengeRepository.deleteById(id);
+            return true;
         }
         return false;
     }
